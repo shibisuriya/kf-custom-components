01ad2fa2cf40a78d44d23301339dc2fdec70d0f0
Shibi <shibisuriya.work@gmail.com>
2024-08-30 15:30:25 +0530

    fix

--- packages/text/src/web/FormField.jsx
+++ packages/text/src/web/FormField.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import { ApiInspector } from "api-inspector";
 
-export const FormField = (props) => {
+export default function Text(props) {
+  const {
+    value,
+    field,
+    actions,
+    disabled,
+    parameter,
+    readonly,
+    errors,
+    theme,
+    fetchKfApi,
+  } = props;
+
+  const { id, name, type, isRequired, hint, defaultValue, color } = field;
+
+  const { updateValue, validateField } = actions;
+
   return (
-    <div>
+    <div style={{ outline: "3px solid red", backgroundColor: color }}>
       <ApiInspector {...props} />
+      <div>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => {
+            const value = e.target.value;
+            updateValue(value);
+          }}
+          onBlur={(e) => {
+            const value = e.target.value;
+            updateValue(value);
+          }}
+        />
+      </div>
     </div>
   );
-};
-
-export default FormField;
+}
